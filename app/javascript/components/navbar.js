a4e343c6518aa8dacaf24f575b0fbe6c834ec7b9
guillermo varela <guillovarela16@gmail.com>
2020-03-17 18:54:41 -0300

    make the banner

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,7 +2,7 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-lewagon');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight-10vh) {
+      if (window.scrollY >= window.innerHeight * 0.8) {
         navbar.classList.add('navbar-lewagon-white');
       } else {
         navbar.classList.remove('navbar-lewagon-white');
