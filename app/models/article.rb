22e7155f20eeecee29424d6c46422392b0bf827d
guillermo varela <guillovarela16@gmail.com>
2020-03-18 09:57:13 -0300

    articles in progress

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true
+  validates :subtitle, presence: true
+  validates :photo, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/show'
-  get 'articles/new'
-  get 'articles/create'
-  get 'articles/destroy'
+
   devise_for :users
   root to: 'pages#home'
+
+  resources :articles
 
   get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
