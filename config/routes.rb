a936da99f1e17fc55db383fb6228c59165321ee7
guillermo varela <guillovarela16@gmail.com>
2020-03-18 09:17:27 -0300

    create articles controlller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+  get 'articles/show'
+  get 'articles/new'
+  get 'articles/create'
+  get 'articles/destroy'
   devise_for :users
   root to: 'pages#home'
 
