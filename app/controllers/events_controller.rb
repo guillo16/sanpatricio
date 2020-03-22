c30434051222022cfc5141b85c9c11329a6813da
guillermo varela <guillovarela16@gmail.com>
2020-03-22 16:43:48 -0300

    finish the event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,47 @@
 class EventsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+
   def index
+    @events = Event.all
   end
 
   def show
   end
 
   def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.user = current_user
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @event.update(event_params)
+    redirect_to event_path(@event)
+  end
+
+  def destroy
+    @event.delete
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:title, :description, :date, :time, :photo)
   end
 end
