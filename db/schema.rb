ab51e023568a8856edee202e3dbb1e393093193b
guillermo varela <guillovarela16@gmail.com>
2020-03-22 16:25:39 -0300

    add user reference to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_22_192139) do
+ActiveRecord::Schema.define(version: 2020_03_22_192508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2020_03_22_192139) do
     t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,4 +54,5 @@ ActiveRecord::Schema.define(version: 2020_03_22_192139) do
   end
 
   add_foreign_key "articles", "users"
+  add_foreign_key "events", "users"
 end
