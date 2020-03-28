7a630b261b689219ae280245d3f0159d46a10115
guillermo varela <guillovarela16@gmail.com>
2020-03-28 14:16:03 -0300

    add validations

--- app/models/conection.rb
+++ app/models/conection.rb
@@ -1,6 +1,6 @@
 class Conection < ApplicationRecord
   validates :name, presence: true
   validates :building, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i }
   validates :message, presence: true
 end
