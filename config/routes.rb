b5a43fcb50b0da34ce6ea39a10ab18d3ebd079cc
guillermo varela <guillovarela16@gmail.com>
2020-03-28 19:03:25 -0300

    user show in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
 
+  get 'users/show'
   devise_for :users
   root to: 'pages#home'
 
@@ -12,5 +13,7 @@ Rails.application.routes.draw do
   get 'ib', to: 'pages#ib'
 
   resources :events
+  resources :users, only: :show
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
