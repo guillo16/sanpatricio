4a6629098453e78fe8fa3b86b36c73487d4c2eca
guillermo varela <guillovarela16@gmail.com>
2020-03-30 19:41:59 -0300

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_28_204809) do
+ActiveRecord::Schema.define(version: 2020_03_30_224129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 2020_03_28_204809) do
     t.datetime "updated_at", null: false
     t.string "category"
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "conections", force: :cascade do |t|
