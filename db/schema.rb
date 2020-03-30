b7a62fa99f08cfdde917ecf1b3b44b316d11bf2a
guillermo varela <guillovarela16@gmail.com>
2020-03-30 20:11:26 -0300

    model post created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_224129) do
+ActiveRecord::Schema.define(version: 2020_03_30_230915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,19 @@ ActiveRecord::Schema.define(version: 2020_03_30_224129) do
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "content"
+    t.string "photo"
+    t.bigint "user_id"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_posts_on_category_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -72,4 +85,6 @@ ActiveRecord::Schema.define(version: 2020_03_30_224129) do
 
   add_foreign_key "articles", "users"
   add_foreign_key "events", "users"
+  add_foreign_key "posts", "categories"
+  add_foreign_key "posts", "users"
 end
