80bb90d3a9471bed288913b20ef600bb234f9672
guillermo varela <guillovarela16@gmail.com>
2020-03-30 12:20:09 -0300

    sweet alert created

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,26 @@
+import { initSweetalert } from '../plugins/init_sweetalert';
+
+initSweetalert('#sweet-alert-demo', {
+  title: "  Estas seguro?",
+  text: "Esta accion no se puede revertir",
+  icon: "warning",
+  buttons: true,
+  dangerMode: true,
+}, (value) => {
+  if (value) {
+    const link = document.querySelector('#delete-link');
+    link.click();
+  }
+  else {
+    swal("Tu mensaje esta seguro");
+  }
+});
+
 import "bootstrap";
 import { loadDynamicBannerText } from '../components/banner';
 loadDynamicBannerText();
+
+
 // import { initUpdateNavbarOnScroll } from '../components/navbar';
 // // initUpdateNavbarOnScroll();
+
