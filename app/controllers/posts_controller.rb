ee5ab80c9bceb5a8d651a7c8573c9d707712f671
guillermo varela <guillovarela16@gmail.com>
2020-03-30 20:22:26 -0300

    finish the forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,32 @@
 class PostsController < ApplicationController
+
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
+    @categories = Category.all.map { |c| [c.title, c.id] }
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.category_id = params[:category_id]
+    @post.user = current_user
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :subtitle, :photo)
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
+  has_many :posts
   validates :title, presence: true
   validates :photo, presence: true
 end
