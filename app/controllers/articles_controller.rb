c2a680f4163fc33b5cbdec61625b2f8c97e9ab3e
guillermo varela <guillovarela16@gmail.com>
2020-04-28 12:11:37 -0300

    Correct the problem with articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @articles = Article.all
+    @articles = Article.order(created_at: :desc)
     @divisions = Division.all
   end
 
