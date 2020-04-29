8e24da4b580864c744b5940ae91d9e788b59ec51
guillermo varela <guillovarela16@gmail.com>
2020-04-29 18:59:39 -0300

    Finish gallery

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :destroy]
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -21,7 +21,16 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :subtitle, :photo, :content, :category_id)
