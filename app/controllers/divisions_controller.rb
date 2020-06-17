d870a00d18db0e85fe336bc5b562bb29783d8c35
guillermo varela <guillovarela16@gmail.com>
2020-06-17 09:57:47 -0300

    Add skip authentication

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -1,4 +1,6 @@
 class DivisionsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
+
   def show
     @division = Division.find(params[:id])
     @divisions = Division.all
