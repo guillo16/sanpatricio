5be238f078c1633c41382f8720e098cab115a3bc
guillermo varela <guillovarela16@gmail.com>
2020-07-17 16:21:04 -0300

    Add bootstrap kaminari

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @users = User.all
+    @users = User.order(:email).page params[:page]
     @categories = Category.all
   end
 
