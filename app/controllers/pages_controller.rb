42a33e99660196a40e0ab371a6c9b30ef4839ff0
guillermo varela <guillovarela16@gmail.com>
2023-06-27 10:26:37 -0300

    Add new routes and controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[home about ib historia autoridades mision]
+  skip_before_action :authenticate_user!, only: %i[home about ib historia autoridades mision inicial primario secundario obras_teatro fiesta_colores salidas_educativas talleres clases_abiertas bicicleteadas]
 
   def home
     @articles = Article.order(created_at: :desc)
@@ -15,4 +15,23 @@ class PagesController < ApplicationController
   def autoridades; end
 
   def mision; end
+
+  def inicial; end
+
+  def primario; end
+
+  def secundario; end
+
+  def obras_teatro; end
+
+  def fiesta_colores; end
+
+  def salidas_educativas; end
+
+  def talleres; end
+
+  def clases_abiertas; end
+
+  def bicicleteadas; end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,15 @@ Rails.application.routes.draw do
   get '/historia', to: 'pages#historia'
   get 'ib', to: 'pages#ib'
   get '/mision', to: 'pages#mision'
+  get '/inicial', to: 'pages#inicial'
+  get '/primario', to: 'pages#primario'
+  get '/secundario', to: 'pages#secundario'
+  get '/obras_teatro', to: 'pages#obras_teatro'
+  get '/fiesta_colores', to: 'pages#fiesta_colores'
+  get '/salidas_educativas', to: 'pages#salidas_educativas'
+  get '/talleres', to: 'pages#talleres'
+  get '/clases_abiertas', to: 'pages#clases_abiertas'
+  get '/bicicleteadas', to: 'pages#bicicleteadas'
 
   resources :articles do
     resources :galleries, only: [:show, :create]
